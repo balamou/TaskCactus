273b6d7ac1899fba28114e670cfbddc18570cead
Michel Balamou <michelbalamou@gmail.com>
2017-10-22 13:00:09 -0400

    Initial commit

---
+++ app/src/main/java/com/uottawa/thirstycactus/taskcactus/CalendarActivity.java
@@ -0,0 +1,13 @@
+package com.uottawa.thirstycactus.taskcactus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
