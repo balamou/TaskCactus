c1852f0717e9d645eade907e6cb48f8014ef49c0
Michel Balamou <michelbalamou@gmail.com>
2017-11-25 00:41:41 -0500

    Added a password box to AddUser

--- app/src/main/java/com/uottawa/thirstycactus/taskcactus/AddUser.java
+++ app/src/main/java/com/uottawa/thirstycactus/taskcactus/AddUser.java
@@ -4,27 +4,43 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 import com.uottawa.thirstycactus.taskcactus.domain.DataSingleton;
+import com.uottawa.thirstycactus.taskcactus.domain.Parent;
 import com.uottawa.thirstycactus.taskcactus.domain.Person;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+
 
 /**
  * Created by Nathanael Adams on 11/21/17.
- *
  */
 
-public class AddUser extends AppCompatActivity
+public class AddUser extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
     // ATTRIBUTES
 
+    private DataSingleton dataSingleton = DataSingleton.getInstance();
+    private int user_id; // id of the user
+
+    // GUI ATTRIBUTES
+
+    private Spinner accountSpinner;
+
     private EditText firstNameEdit;
     private EditText lastNameEdit;
+    private EditText birthDayEdit;
+    private EditText passwordText;
 
-    private DataSingleton dataSingleton = DataSingleton.getInstance();
-    private int user_id;
+    private LinearLayout parentLayout;
+    private LinearLayout accountLayout;
 
     // =============================================================================================
 
@@ -41,18 +57,85 @@ public class AddUser extends AppCompatActivity
         // INITIALIZE THE GRAPHICAL OBJECTS
         firstNameEdit = (EditText) findViewById(R.id.firstNameEdit);
         lastNameEdit = (EditText) findViewById(R.id.lastNameEdit);
+        passwordText = (EditText) findViewById(R.id.passwordText);
+        birthDayEdit = (EditText) findViewById(R.id.birthDayEdit);
 
+        parentLayout = (LinearLayout) findViewById(R.id.parentLayout);
+        accountLayout = (LinearLayout) findViewById(R.id.accountLayout);
+
+        // SET UP SPINNER
+        accountSpinner = (Spinner) findViewById(R.id.accountSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.account_type, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        accountSpinner.setAdapter(adapter);
+        accountSpinner.setOnItemSelectedListener(this);
+
+
+        // CAPTURE THE FLAG SENT FROM PREVIOUS ACTIVITY
+        loadUserInfo();
+    }
+
+    /**
+     * Loads user information if a user was selected in the previous activity
+     */
+    public void loadUserInfo()
+    {
         // CAPTURE THE FLAG SENT FROM PREVIOUS ACTIVITY
         Intent intent = getIntent();
         user_id = intent.getIntExtra("USER_ID", -2);
 
         if (user_id>=0) // FLAG: EDIT CURRENT USER
         {
+            accountLayout.setVisibility(View.GONE); // HIDE THE ACCOUNT TYPE CHANGE
             Person user = dataSingleton.getUsers().get(user_id);
 
             firstNameEdit.setText(user.getFirstName());
             lastNameEdit.setText(user.getLastName());
+
+            // DISPLAY BIRTH DAY
+            Date birth = user.getBirthDate();
+
+            if (birth!=null)
+            {
+                SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+                birthDayEdit.setText(format.format(birth));
+            }
+
+            // SHOW/HIDE password box
+            if (user instanceof Parent) // show the password box only if the type of user is parent
+            {
+                accountSpinner.setSelection(1);
+                parentLayout.setVisibility(View.VISIBLE);
+                passwordText.setText(((Parent) user).getHashedPIN());
+            }
         }
+    }
+
+    // =============================================================================================
+
+    // LISTENERS
+
+    // =============================================================================================
+
+    /**
+     * When changing the selection in the spinner;
+     * Show/hide the password box
+     */
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
+    {
+        if (parent.getItemAtPosition(pos).equals("Child"))
+        {
+            parentLayout.setVisibility(View.GONE);
+        }
+        else if (parent.getItemAtPosition(pos).equals("Parent"))
+        {
+            parentLayout.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void onNothingSelected(AdapterView<?> parent)
+    {
+        // Nothing happens
     }
 
     /**
@@ -76,7 +159,7 @@ public class AddUser extends AppCompatActivity
         // CHECKS IF THE DATA IS VALID BEFORE SUBMITTING IT
         if (firstName!=null && !firstName.isEmpty() && lastName!=null && !lastName.isEmpty())
         {
-            if (user_id == -1) // FLAG ADD NEW USER
+            if (user_id == -1) // FLAG: ADD NEW USER
             {
                 Person newUser = new Person(firstName, lastName);
 
