177ae342794a9e581155767f672c296a65bbb579
Peter Nguyen <Bobbyle@outlook.com>
2017-11-27 12:52:22 -0500

    Shows resource fields when clicking add resource button

--- app/src/main/java/com/uottawa/thirstycactus/taskcactus/ResourcesActivity.java
+++ app/src/main/java/com/uottawa/thirstycactus/taskcactus/ResourcesActivity.java
@@ -2,6 +2,9 @@ package com.uottawa.thirstycactus.taskcactus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.uottawa.thirstycactus.taskcactus.adapters.ResAdapter;
@@ -13,6 +16,9 @@ public class ResourcesActivity extends AppCompatActivity
     // ATTRIBUTES
 
     private ListView resourcesListView;
+
+    private LinearLayout newResLayout;
+    private Button addBtn;
 
 
     // =============================================================================================
@@ -28,8 +34,31 @@ public class ResourcesActivity extends AppCompatActivity
         setContentView(R.layout.activity_resources);
 
 
+        newResLayout = (LinearLayout) findViewById(R.id.newResLayout);
+        addBtn = (Button) findViewById(R.id.addBtn);
+
         resourcesListView = (ListView) findViewById(R.id.resourcesListView);
         ResAdapter resAdapter = new ResAdapter(this, DataSingleton.getInstance().getResources());
         resourcesListView.setAdapter(resAdapter);
+    }
+
+    public void showResFields(View view)
+    {
+        if (newResLayout.getVisibility() == View.GONE)
+        {
+            newResLayout.setVisibility(View.VISIBLE);
+            addBtn.setText("-");
+        }
+        else
+        {
+            newResLayout.setVisibility(View.GONE);
+            addBtn.setText("+");
+        }
+    }
+
+    public void onCancel(View view)
+    {
+        newResLayout.setVisibility(View.GONE);
+        addBtn.setText("+");
     }
 }
