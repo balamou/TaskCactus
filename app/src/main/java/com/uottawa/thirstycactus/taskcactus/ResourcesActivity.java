16f00674ae01b799265475708e6a17e997713820
Peter Nguyen <Bobbyle@outlook.com>
2017-11-27 14:26:02 -0500

    Can add a new resource.

--- app/src/main/java/com/uottawa/thirstycactus/taskcactus/ResourcesActivity.java
+++ app/src/main/java/com/uottawa/thirstycactus/taskcactus/ResourcesActivity.java
@@ -4,11 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.uottawa.thirstycactus.taskcactus.adapters.ResAdapter;
 import com.uottawa.thirstycactus.taskcactus.domain.DataSingleton;
+import com.uottawa.thirstycactus.taskcactus.domain.Resource;
 
 public class ResourcesActivity extends AppCompatActivity
 {
@@ -16,9 +19,13 @@ public class ResourcesActivity extends AppCompatActivity
     // ATTRIBUTES
 
     private ListView resourcesListView;
+    private ResAdapter resAdapter;
 
     private LinearLayout newResLayout;
     private Button addBtn;
+
+    private EditText nameEdit;
+    private EditText descEdit;
 
 
     // =============================================================================================
@@ -36,12 +43,19 @@ public class ResourcesActivity extends AppCompatActivity
 
         newResLayout = (LinearLayout) findViewById(R.id.newResLayout);
         addBtn = (Button) findViewById(R.id.addBtn);
+        nameEdit = (EditText) findViewById(R.id.nameEdit);
+        descEdit = (EditText) findViewById(R.id.descEdit);
+
 
         resourcesListView = (ListView) findViewById(R.id.resourcesListView);
-        ResAdapter resAdapter = new ResAdapter(this, DataSingleton.getInstance().getResources());
+        resAdapter = new ResAdapter(this, DataSingleton.getInstance().getResources());
         resourcesListView.setAdapter(resAdapter);
     }
 
+    /**
+     * SHOWS/HIDES the from 'add a resource';
+     * It is located right above the listview.
+     */
     public void showResFields(View view)
     {
         if (newResLayout.getVisibility() == View.GONE)
@@ -56,9 +70,42 @@ public class ResourcesActivity extends AppCompatActivity
         }
     }
 
+    /**
+     * Hides the form 'add a resource'
+     */
     public void onCancel(View view)
     {
         newResLayout.setVisibility(View.GONE);
         addBtn.setText("+");
+    }
+
+    /**
+     * Saves a new resource
+     */
+    public void onSave(View view)
+    {
+        String name = nameEdit.getText().toString();
+        String desc = descEdit.getText().toString();
+
+        if (name.isEmpty())
+        {
+            Toast.makeText(getApplicationContext(), "Please enter a resource name", Toast.LENGTH_SHORT).show();
+            return ;
+        }
+
+        Resource res = new Resource(name, desc);
+
+        DataSingleton.getInstance().getResources().add(res);
+
+        Toast.makeText(getApplicationContext(), "Resource '" + name + "' added", Toast.LENGTH_SHORT).show();
+
+        // REFRESH LISTVIEW
+        resAdapter.notifyDataSetChanged();
+
+        // RESET FORMS
+        newResLayout.setVisibility(View.GONE);
+        addBtn.setText("+");
+        nameEdit.setText("");
+        descEdit.setText("");
     }
 }
